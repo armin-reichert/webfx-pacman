67f058ab3a012ffe43b623ffa2ba6e30f6dd5858
Armin Reichert <armin.reichert@web.de>
2023-11-14 19:40:13 +0100

    Update with latest webfx

--- webfx-pacman-application-gluon/src/main/java/module-info.java
+++ webfx-pacman-application-gluon/src/main/java/module-info.java
@@ -6,20 +6,17 @@ module webfx.pacman.application.gluon {
     requires javafx.media;
     requires webfx.kit.openjfx;
     requires webfx.pacman.application;
+    requires webfx.platform.audio.gluon;
     requires webfx.platform.boot.java;
     requires webfx.platform.console.java;
-    requires webfx.platform.json.java;
     requires webfx.platform.os.gluon;
     requires webfx.platform.resource.gluon;
     requires webfx.platform.scheduler.java;
     requires webfx.platform.shutdown.gluon;
     requires webfx.platform.storage.java;
     requires webfx.platform.storagelocation.gluon;
+    requires webfx.platform.substitution.var;
     requires webfx.platform.visibility.gluon;
-    requires webfx.platform.windowhistory.java;
     requires webfx.platform.windowlocation.java;
-
-    // Meta Resource package
-    opens dev.webfx.platform.meta.exe;
 
 }
